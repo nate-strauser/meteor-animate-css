38776ed39230f7fbddd7c371dbc6133a2b48c980
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 20:40:07 -0400

    0.9.0 update

--- package.js
+++ package.js
@@ -1,7 +1,11 @@
 Package.describe({
-    summary: "Animate.css packaged for meteor"
+    name:"natestrauser:animate-css",
+    summary: "Animate.css packaged for meteor",
+  version: "3.1.2",
+  git: "https://github.com/nate-strauser/meteor-animate-css.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.add_files("lib/animate.css/animate.css", "client");
 });
